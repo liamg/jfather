79a58f18e4186aa57a42810214e5e505f654a3b1
Liam Galvin <liamgalvin@protonmail.com>
2021-08-29 22:16:43 +0100

    Added test using example from json.org

---
+++ parse_complex_test.go
@@ -0,0 +1,34 @@
+package jfather
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Complex(t *testing.T) {
+	target := make(map[string]interface{})
+	input := `{
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}`
+	require.NoError(t, Unmarshal([]byte(input), &target))
+}
